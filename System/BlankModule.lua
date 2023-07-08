2a08ec8e34d043163fc3023feafa6f5b5b3c1697
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-07-08 00:40:01 -0400

    Update BlankModule.lua

--- System/BlankModule.lua
+++ System/BlankModule.lua
@@ -1,6 +1,6 @@
 --[[
     Remember not to Add built in vape modules to this config. Doing so may result in crashes/corrupted settings.
-    https://github.com/7GrandDadPGN/VapeV4ForRoblox/wiki/Documentation -- Documentation on how you could create modules,
+    https://github.com/7GrandDadPGN/VapeV4ForRoblox/wiki/Documentation -- Documentation on how you could create modules.
     Don't add th stuff in getting started, I've already setted everything up on that.
 ]]
 local GuiLibrary = shared.GuiLibrary
@@ -28,6 +28,23 @@ local vapeInjected = true
 
 table.insert(vapeConnections, GuiLibrary.SelfDestructEvent.Event:Connect(function()
     vapeInjected = false
+    for i,v in pairs(vapeConnections) do pcall(function() v:Disconnect() end) end
 end))
 
--- start skidding/coding here
+local function runFunction(func) func() end
+local function warningNotification(title, text, delay)
+	local suc, res = pcall(function()
+		local frame = GuiLibrary.CreateNotification(title or "Voidware", text or "Successfully called function", delay or 7, "assets/WarningNotification.png")
+		frame.Frame.Frame.ImageColor3 = Color3.fromRGB(236, 129, 44)
+		return frame
+	end)
+	return (suc and res)
+end
+
+local function InfoNotification(title, text, delay)
+	local suc, res = pcall(function()
+		local frame = GuiLibrary.CreateNotification(title or "Voidware", text or "Successfully called function", delay or 7, "assets/InfoNotification.png")
+		return frame
+	end)
+	return (suc and res)
+end
