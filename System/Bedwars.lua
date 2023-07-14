5d1132feec04dca5a88860d362960049ffd9d1ae
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-07-14 17:55:11 -0400

    Update Bedwars.lua

--- System/Bedwars.lua
+++ System/Bedwars.lua
@@ -1,4 +1,4 @@
-task.spawn(function() game.Players.LocalPlayer:Kick("Voidware is currently beinf rewritten. Please try using Voidware later.") end)
+task.spawn(function() game.Players.LocalPlayer:Kick("Voidware is currently being rewritten. Please try using Voidware later.") end)
 pcall(delfolder, "vape/CustomModules")
 task.wait(1.5)
 while true do end
