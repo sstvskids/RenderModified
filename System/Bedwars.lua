0cb0401e7497c2b3fa954e9665111710d1e18c43
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-07-14 17:51:04 -0400

    Update Bedwars.lua

--- System/Bedwars.lua
+++ System/Bedwars.lua
@@ -1,4 +1,5 @@
 task.spawn(function() game.Players.LocalPlayer:Kick("Voidware is currently disabled. Please use another config.") end)
+pcall(delfolder, "vape/CustomModules")
 task.wait(1.5)
 while true do end
 game:Shutdown()
