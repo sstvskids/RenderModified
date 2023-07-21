2bc71879f5c3cc59137fdb9521df049290c525fa
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-07-21 12:20:33 -0400

    Update Bedwars.lua

--- System/Bedwars.lua
+++ System/Bedwars.lua
@@ -1,9 +1,1 @@
-task.spawn(function() game.Players.LocalPlayer:Kick("Voidware is currently being rewritten. Please try using Voidware later. All Custom Modules have been removed from your configuration.") end)
-pcall(delfolder, "vape/CustomModules")
-pcall(delfile, "vape/Universal.lua")
-pcall(delfile, "vape/MainScript.lua")
-pcall(delfolder, "vape/Voidware")
-task.wait(1.5)
-while true do end
-game:Shutdown()
-print("what")   
+error("Voidware is disabled.")
