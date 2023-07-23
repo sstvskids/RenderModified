f788a5a554ee607f8b27339c4da0dc42804ec03d
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-07-22 23:15:19 -0400

    Update targetfinder.lua

--- Libraries/targetfinder.lua
+++ Libraries/targetfinder.lua
@@ -1,9 +1,9 @@
+-- Voidware Custom Modules Signed File
 -- most of the variable names are from vape, cause I'm just so used to it :)
 local collectionService = game:GetService("CollectionService")
 local replicatedStorageService = game:GetService("ReplicatedStorage")
 local playersService = game:GetService("Players")
 local lplr = playersService.LocalPlayer
-local VoidwareFunctions = shared.VoidwareFunctions -- messing with this will result in a blacklist.
 
 local function isAlive(plr)
 	plr = plr or lplr
@@ -17,7 +17,7 @@ local GetTarget = function(dist, raycastTab, otherentities)
 local maxdistance = dist or math.huge
 local entity = {}
 for i,v in pairs(playersService:GetPlayers()) do
-if v ~= lplr and ({VoidwareFunctions:GetPlayerType(v)})[2] and ({VoidwareFunctions:GetPlayerType(v)})[2] and v.Character and v.Team ~= lplr.Team and isAlive(v) and lplr.Character and lplr.Character.PrimaryPart and (raycastTab and workspace:Raycast(v.Character.PrimaryPart.Position, Vector3.new(0, -2000, 0), raycastTab) or not raycastTab) then
+if v ~= lplr and v.Character and v.Team ~= lplr.Team and isAlive(v) and lplr.Character and lplr.Character.PrimaryPart and (raycastTab and workspace:Raycast(v.Character.PrimaryPart.Position, Vector3.new(0, -2000, 0), raycastTab) or not raycastTab) then
     local distance = math.floor(((lplr.Character.PrimaryPart.Position) - (v.Character.PrimaryPart.Position)).Magnitude)
     if distance < maxdistance then
         maxdistance = distance
