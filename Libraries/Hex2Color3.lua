1afe9ee17d7867abd701a29a875920c1aa15a391
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-08-31 03:05:03 -0400

    Update Hex2Color3.lua

--- Libraries/Hex2Color3.lua
+++ Libraries/Hex2Color3.lua
@@ -1,8 +1,13 @@
+-- Voidware Custom Modules Signed File
 local LibraryFunctions = {}
-
 LibraryFunctions.GetColor3 = function(hex)
     hex = hex and string.gsub(hex, "#", "") or "FFFFFF"
     return Color3.new(tonumber("0x"..hex:sub(1,2)), tonumber("0x"..hex:sub(3,4)), tonumber("0x"..hex:sub(5,6)))
+end
+
+LibraryFunctions.UnpackColor3 = function(hex)
+    hex = hex and string.gsub(hex, "#", "") or "FFFFFF"
+    return tonumber("0x"..hex:sub(1,2)), tonumber("0x"..hex:sub(3,4)), tonumber("0x"..hex:sub(5,6))
 end
 
 return LibraryFunctions
