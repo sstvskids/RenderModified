16c0e463b459f29d1357e9654ee8a824182b6613
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-08-31 02:28:49 -0400

    Update Hex2Color3.lua

--- Libraries/Hex2Color3.lua
+++ Libraries/Hex2Color3.lua
@@ -1,6 +1,6 @@
 local LibraryFunctions = {}
 
-LibraryFunctions.Hex2Color3 = function(hex)
+LibraryFunctions.GetColor3 = function(hex)
     hex = hex and string.gsub(hex, "#", "") or "FFFFFF"
     return Color3.new(tonumber("0x"..hex:sub(1,2)), tonumber("0x"..hex:sub(3,4)), tonumber("0x"..hex:sub(5,6)))
 end
