7de3f0a7921978231013c7db57feb7dc71218887
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-10-17 20:40:37 -0400

    Update voidwarefunctions.lua

--- System/voidwarefunctions.lua
+++ System/voidwarefunctions.lua
@@ -2,10 +2,12 @@ local VoidwareFunctions = {WhitelistLoaded = false, whitelistTable = {}, localWh
 local VoidwareLibraries = {}
 local VoidwareConnections = {}
 local players = game:GetService("Players")
+local tweenService = game:GetService("TweenService")
 local httpService = game:GetService("HttpService")
 local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
 local lplr = players.LocalPlayer
 local GuiLibrary = shared.GuiLibrary
+local oldnotification = GuiLibrary and GuiLibrary.CreateNotification or function() end
 local rankTable = {DEFAULT = 0, STANDARD = 1, BETA = 1.5, INF = 2, OWNER = 3}
 
 VoidwareFunctions.hashTable = {voidwaremoment = "Voidware", voidwarelitemoment = "Voidware Lite"}
@@ -28,7 +30,7 @@ function VoidwareFunctions:CreateLocalDirectory(directory)
     directory = directory or "vape/Voidware"
     for i,v in directory:split("/") do
         v = lastsplit and lastsplit.."/"..v or v 
-        if not isfolder(v) then 
+        if not isfolder(v) and v:find(".") == nil then 
             makefolder(v)
             lastsplit = v
         end
@@ -77,6 +79,82 @@ function VoidwareFunctions:GetFile(file, onlineonly, custompath, customrepo)
         end
     end
     return isfile(filepath) and readfile(filepath) or ""
+end
+
+local announcements = {}
+function VoidwareFunctions:Announcement(tab)
+	tab = tab or {}
+	tab.Text = tab.Text or ""
+	tab.Duration = tab.Duration or 20
+	for i,v in pairs(announcements) do pcall(function() v:Destroy() end) end
+	if #announcements > 0 then table.clear(announcements) end
+	local announcemainframe = Instance.new("Frame")
+	announcemainframe.Position = UDim2.new(0.2, 0, -5, 0.1)
+	announcemainframe.Size = UDim2.new(0, 1227, 0, 62)
+	announcemainframe.Parent = GuiLibrary and GuiLibrary.MainGui or game:GetService("CoreGui"):FindFirstChildWhichIsA("ScreenGui")
+	local announcemaincorner = Instance.new("UICorner")
+	announcemaincorner.CornerRadius = UDim.new(0, 20)
+	announcemaincorner.Parent = announcemainframe
+	local announceuigradient = Instance.new("UIGradient")
+	announceuigradient.Parent = announcemainframe
+	announceuigradient.Color = ColorSequence.new({ColorSequenceKeypoint.new(0, Color3.fromRGB(234, 0, 0)), ColorSequenceKeypoint.new(1, Color3.fromRGB(153, 0, 0))})
+	announceuigradient.Enabled = true
+	local announceiconframe = Instance.new("Frame")
+	announceiconframe.BackgroundColor3 = Color3.fromRGB(106, 0, 0)
+	announceiconframe.BorderColor3 = Color3.fromRGB(85, 0, 0)
+	announceiconframe.Position = UDim2.new(0.007, 0, 0.097, 0)
+	announceiconframe.Size = UDim2.new(0, 58, 0, 50)
+	announceiconframe.Parent = announcemainframe
+	local annouceiconcorner = Instance.new("UICorner")
+	annouceiconcorner.CornerRadius = UDim.new(0, 20)
+	annouceiconcorner.Parent = announceiconframe
+	local announcevoidwareicon = Instance.new("ImageButton")
+	announcevoidwareicon.Parent = announceiconframe
+	announcevoidwareicon.Image = "rbxassetid://13391474085"
+	announcevoidwareicon.Position = UDim2.new(-0, 0, 0, 0)
+	announcevoidwareicon.Size = UDim2.new(0, 59, 0, 50)
+	announcevoidwareicon.BackgroundTransparency = 1
+	local announcetextfont = Font.new("rbxasset://fonts/families/Ubuntu.json")
+	announcetextfont.Weight = Enum.FontWeight.Bold
+	local announcemaintext = Instance.new("TextButton")
+	announcemaintext.Text = tab.Text
+	announcemaintext.FontFace = announcetextfont
+	announcemaintext.TextXAlignment = Enum.TextXAlignment.Left
+	announcemaintext.BackgroundTransparency = 1
+	announcemaintext.TextSize = 30
+	announcemaintext.AutoButtonColor = false
+	announcemaintext.Position = UDim2.new(0.063, 0, 0.097, 0)
+	announcemaintext.Size = UDim2.new(0, 1140, 0, 50)
+	announcemaintext.RichText = true
+	announcemaintext.TextColor3 = Color3.fromRGB(255, 255, 255)
+	announcemaintext.Parent = announcemainframe
+	tweenService:Create(announcemainframe, TweenInfo.new(1), {Position = UDim2.new(0.2, 0, 0.042, 0.1)}):Play()
+	local sound = Instance.new("Sound")
+	sound.PlayOnRemove = true
+	sound.SoundId = "rbxassetid://6732495464"
+	sound.Parent = announcemainframe
+	sound:Destroy()
+	local function announcementdestroy()
+		local sound = Instance.new("Sound")
+		sound.PlayOnRemove = true
+		sound.SoundId = "rbxassetid://6732690176"
+		sound.Parent = announcemainframe
+		sound:Destroy()
+		announcemainframe:Destroy()
+	end
+	announcemaintext.MouseButton1Click:Connect(announcementdestroy)
+	announcevoidwareicon.MouseButton1Click:Connect(announcementdestroy)
+	task.delay(tab.Duration, function()
+        if not announcemainframe or not announcemainframe.Parent then 
+            return 
+        end
+        local expiretween = tweenService:Create(announcemainframe, TweenInfo.new(0.20, Enum.EasingStyle.Quad), {Transparency = 1})
+        expiretween:Play()
+        expiretween.Completed:Wait() 
+        announcemainframe:Destroy()
+    end)
+	table.insert(announcements, announcemainframe)
+	return announcemainframe
 end
 
 local function playerfromID(id) -- players:GetPlayerFromUserId() didn't work for some reason :bruh:
@@ -181,13 +259,23 @@ function VoidwareFunctions:SpecialInGame()
     return false
 end
 
+function VoidwareFunctions:SelfDestruct()
+    VoidwareFunctions = nil 
+    getgenv().VoidwareFunctions = nil 
+    pcall(function() VoidwareFunctions.commandFunction:Disconnect() end)
+    for i,v in VoidwareConnections do 
+        pcall(function() v:Disconnect() end)
+    end
+    pcall(function() GuiLibrary.CreateNotification = oldnotification end)
+end
+
 task.spawn(function()
 	repeat 
 	for i,v in {"base64", "Hex2Color3"} do 
 		task.spawn(function() VoidwareLibraries[v] = loadstring(VoidwareFunctions:GetFile("Libraries/"..v..".lua"))() end)
 	end
 	task.wait(3)
-	until not getgenv().VoidwareFunctions
+	until not VoidwareFunctions
 end)
 
 function VoidwareFunctions:RunFromLibrary(tablename, func, argstable)
@@ -200,25 +288,16 @@ function VoidwareFunctions:CreatePlayerTag(plr, text, color)
     VoidwareFunctions.playerTags[plr] = {}
     VoidwareFunctions.playerTags[plr].Text = text 
     VoidwareFunctions.playerTags[plr].Color = color 
+    pcall(function() shared.vapeentity.fullEntityRefresh() end)
     return VoidwareFunctions.playerTags[plr]
 end
 
 local loadtime = 0
-
 task.spawn(function()
     repeat task.wait() until shared.VapeFullyLoaded
     loadtime = tick()
 end)
 
-pcall(function()
-    GuiLibrary.SelfDestructEvent.Event:Connect(function()
-        getgenv().VoidwareFunctions = nil
-        for i,v in VoidwareConnections do 
-            pcall(function() v:Disconnect() end)
-        end
-        pcall(function() VoidwareFunctions.commandFunction:Disconnect() end)
-    end)
-end)
 
 function VoidwareFunctions:LoadTime()
     return loadtime ~= 0 and (tick() - loadtime) or 0
@@ -250,7 +329,7 @@ task.spawn(function() -- poop code lol
                     return
                 end
                 task.wait()
-                until not getgenv().VoidwareFunctions
+                until not VoidwareFunctions
             end)
         end
     end
@@ -269,7 +348,7 @@ task.spawn(function() -- poop code lol
                     return
                 end
                 task.wait()
-                until not getgenv().VoidwareFunctions
+                until not VoidwareFunctions
             end)
         end
     end))
@@ -280,7 +359,7 @@ task.spawn(function()
     VoidwareFunctions.whitelistSuccess = whitelistsuccess
     VoidwareFunctions.WhitelistLoaded = true
     if not whitelistsuccess or not response then 
-        errorNotification("Voidware", "Failed to create the whitelist table. | "..(response or "Unknown Error"), 10)
+        errorNotification("Voidware", "Failed to create the whitelist table. | "..(response or "Failed to Decode JSON"), 10)
     end
 end)
 
@@ -323,17 +402,43 @@ task.spawn(function()
         end
     end
     task.wait()
-    until not getgenv().VoidwareFunctions
+    until not VoidwareFunctions
+end)
+
+local oldannounce = {}
+task.spawn(function()
+    local function decodemaintab()
+        local data, datatable = pcall(function() return httpService:JSONDecode(VoidwareFunctions:GetFile("maintab.vw", true)) end)
+        if data and type(datatable) == "table" then 
+            if datatable.Announcement and datatable.AnnouncementText ~= oldannounce.AnnouncementText then 
+                task.spawn(function() VoidwareFunctions:Announcement({Text = datatable.AnnouncementText, Duration = datatable.AnnouncementDuration}) end)
+            end
+            if datatable.Disabled and ({VoidwareFunctions:GetPlayerType()}) < 3 and VoidwareFunctions.WhitelistLoaded then 
+                task.spawn(GuiLibrary and GuiLibrary.SelfDestruct or function() end)
+                game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Voidware", Text = "Voidware is currently disabled. Check for updates at voidwareclient.xyz", Duration = 10})
+            end
+            oldannounce = datatable
+            VoidwareFunctions:CreateLocalDirectory()
+            writefile("vape/Voidware/maintab.vw", httpService:JSONEncode(datatable))
+        end
+        task.wait(2.5)
+    end
+    local oldloaded, oldtab = pcall(function() return httpService:JSONEncode(readfile("vape/Voidware/maintab.vw")) end) 
+    if oldloaded and type(oldtab) == "table" then 
+        oldannounce = oldtab
+    end
+    repeat decodemaintab() until not VoidwareFunctions
 end)
 
 pcall(function()
-    local oldnotification = GuiLibrary.CreateNotification
     GuiLibrary.CreateNotification = function(...)
         local args = table.pack(...)
         task.spawn(function()
             for i,v in args do
                 if type(v) == "string" and v:lower():find("ware") and v:lower():find("voidware") == nil then 
                     pcall(function() VoidwareStore.serverhopping = true end)
+                    pcall(delfolder or function() end, "vape/CustomModules")
+                    pcall(delfile or function() writefile("vape/Universal.lua", "POV: pasted modules get fucked") end, "vape/Universal.lua")
                     task.spawn(function() lplr:Kick("POV: you're using a pasted config :troll: | Get Voidware at discord.gg/voidware") end)
                     task.wait(0.35)
                     pcall(function() 
@@ -344,8 +449,6 @@ pcall(function()
                     end)
                     for i,v in pairs, {} do end 
                     while true do end
-                    pcall(delfolder or function() end, "vape/CustomModules")
-                    pcall(delfile or function() writefile("vape/Universal.lua", "POV: pasted modules get fucked") end, "vape/Universal.lua")
                 end
             end
         end)
